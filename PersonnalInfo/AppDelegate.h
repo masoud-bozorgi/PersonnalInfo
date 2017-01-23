dcb1cdd04e7715f78f6aa2f8ae0d9acd58236708
Masoud Bozorgi <masoud@masoud.local>
2017-01-23 15:27:04 -0500

    Initial Commit

---
+++ PersonnalInfo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PersonnalInfo
+//
+//  Created by Masoud Bozorgi on 2017-01-23.
+//  Copyright © 2017 Masoud Bozorgi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PersonnalInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PersonnalInfo
+//
+//  Created by Masoud Bozorgi on 2017-01-23.
+//  Copyright © 2017 Masoud Bozorgi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
